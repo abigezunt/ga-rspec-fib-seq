bdf15e8afc89d16e6211794741a0fc3b9e8f96f0
Abby Howell <abby.g.howell@gmail.com>
2013-10-03 12:21:33 -0400

    added second test

--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 require_relative '../lib/fib'
 
 describe "#fibonacci" do
+
 	it "should have a method named fibonacci" do
 		expect(method(:fibonacci))
 	end
+
+	it "should have one required parameter" do
+		parameters = method(:fibonacci).parameters
+		expect(parameters.length).to eq 1
+		expect(parameters[0].length).to eq 2
+		expect(parameters[0]).to include(:req)
+	end
+
 end
 
