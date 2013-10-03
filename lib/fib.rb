09700fcecc0751bba59dd55c53129070b70be3c2
Abby Howell <abby.g.howell@gmail.com>
2013-10-03 12:43:35 -0400

    five tests, all working

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,2 +1,12 @@
 def fibonacci(num)
+	arr = [1, 1]
+	if num <=2
+		arr.take(num)
+	else
+		loop do
+		arr << (arr[(arr.length - 2)] + arr.last)
+		break if arr.length > num
+		end
+	end
+	return arr.take(num)
 end
--- spec/fib_spec.rb
+++ spec/fib_spec.rb
@@ -14,5 +14,22 @@ describe "#fibonacci" do
 		expect(parameters[0]).to include(:req)
 	end
 
+	it "should return an array" do
+		expect(fibonacci(3).class).to be Array
+	end
+
+	it "should return num digits of the fibonacci sequence" do
+		expect(fibonacci(8).length).to eq 8
+	end
+
+	it "should return the fibonacci sequence" do
+	  expect(fibonacci(4)).to eq [1, 1, 2, 3]
+	  expect(fibonacci(9)).to eq [1, 1, 2, 3, 5, 8, 13, 21, 34]
+	  expect(fibonacci(0)).to eq []
+	  expect(fibonacci(1)).to eq [1]
+	  expect(fibonacci(2)).to eq [1, 1]
+	end
+
+
 end
 
